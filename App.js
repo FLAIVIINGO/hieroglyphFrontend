d63cb2858eaccd9bccb8601dbc26f9abdcb05be7
Andrew Babilonia <dodongo@Andrews-MacBook-Air.local>
2024-02-28 11:39:22 -0500

    improved card change

--- App.js
+++ App.js
@@ -7,15 +7,22 @@ import AdvanceCardButton from './components/AdvanceCardButton/AdvanceCardButton'
 
 const App = () => {
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [isFlipped, setIsFlipped] = useState(false);
   const changeCard = () => {
     const newIndex = Math.floor(Math.random() * EXAMPLE_FLASHCARDS.length);
     setCurrentCardIndex(newIndex);
+    setIsFlipped(false);
   };
+  const flipCard = () => setIsFlipped(!isFlipped);
 
   return (
     <SafeAreaView style={globalStyle.container}>
       <View style={globalStyle.cardContainer}>
-        <Flashcard {...EXAMPLE_FLASHCARDS[currentCardIndex]} />
+        <Flashcard
+          {...EXAMPLE_FLASHCARDS[currentCardIndex]}
+          isFlipped={isFlipped}
+          flipCard={flipCard}
+        />
       </View>
       <View>
         <AdvanceCardButton
--- components/Flashcard/Flashcard.js
+++ components/Flashcard/Flashcard.js
@@ -1,17 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import PropTypes from 'prop-types';
 import style from './style';
 
 const Flashcard = props => {
-  const [isFlipped, setIsFlipped] = useState(false);
-
-  const flipCard = () => setIsFlipped(!isFlipped);
-
   return (
-    <TouchableOpacity onPress={flipCard} style={style.card}>
+    <TouchableOpacity onPress={props.flipCard} style={style.card}>
       <View>
-        {isFlipped ? (
+        {props.isFlipped ? (
           <Text style={style.text}>{props.answer}</Text>
         ) : (
           <Text style={style.text}>{props.question}</Text>
@@ -24,6 +20,8 @@ const Flashcard = props => {
 Flashcard.propTypes = {
   question: PropTypes.string.isRequired,
   answer: PropTypes.string.isRequired,
+  isFlipped: PropTypes.bool.isRequired,
+  flipCard: PropTypes.func.isRequired,
 };
 
 export default Flashcard;
